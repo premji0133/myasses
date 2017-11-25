9e0f5659b0e7f9c09ac877eaf30da717cc82894c
premji0133 <30617577+premji0133@users.noreply.github.com>
2017-11-25 09:07:02 +0530

    Update dbcon.ts

--- server/routes/dbcon.ts
+++ server/routes/dbcon.ts
@@ -3,7 +3,7 @@ import { Sequelize } from "sequelize";
 
 
 
-var sequelize = new Sequelize('postgres://gvoqmzur:xUjOhXIesEfXJwh97h2qDY36LNTBeKrE@pellefant.db.elephantsql.com:5432/gvoqmzur');
+var sequelize = new Sequelize('postgres://etaqmobqletrrd:01e51d39889a89dd5aa428ee93de45dc15a2f5c7c4b20f4d6d732256df9b55df@ec2-184-73-189-190.compute-1.amazonaws.com:5432/ddr69qdleugjll');
 
 
 export {   Sequelize, sequelize };
