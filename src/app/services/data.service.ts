c5304ee6b9f4cf0566c8230513cc7f757cdaa996
premji0133 <30617577+premji0133@users.noreply.github.com>
2017-12-04 16:39:37 +0530

    Create data.service.ts

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,43 @@
+import { Injectable }    from '@angular/core';
+import { Headers, Http, Response, RequestOptionsArgs } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+import { marks } from './mock';
+@Injectable()
+export class DataService 
+{
+   constructor(private http: Http) { }
+  
+   getuserlist()  : Promise<Parse[]> {
+    
+
+
+               return this.http.get('https://destiny.mod.bz/listusers')
+                  .toPromise()
+                  .then(res => <Parse[]> res.json(), this.handleError)
+                  .then(data => { console.log(data);  return data; }); 
+
+
+   }
+
+
+   getdriverlist() : Promise<Driver[]> {
+    
+
+
+               return this.http.get('https://destiny.mod.bz/driverlist')
+                  .toPromise()
+                  .then(res => <Driver[]> res.json(), this.handleError)
+                  .then(data => { console.log(data); return data; }); 
+                  
+
+                                 
+   }
+   
+   
+   
+     private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
+
+  }
