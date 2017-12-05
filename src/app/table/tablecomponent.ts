0095b08fd91a61b823fb70ab52ce067380056161
premji0133 <30617577+premji0133@users.noreply.github.com>
2017-12-05 09:29:04 +0530

    Update tablecomponent.ts

--- src/app/table/tablecomponent.ts
+++ src/app/table/tablecomponent.ts
@@ -49,7 +49,7 @@ export interface Elements {
 
  ELEMENT_DATA:  = {"users":[{"ID":125,"NAME":"Prems021","TESTNUM":1,"MARK":1},
                             {"ID":126,"NAME":"Prems021","TESTNUM":1,"MARK":4},
-                            {"ID":127,"NAME":"Prems021","TESTNUM":1,"MARK":4}]}
+                            {"ID":127,"NAME":"Prems021","TESTNUM":1,"MARK":4}]};
   
   
 
