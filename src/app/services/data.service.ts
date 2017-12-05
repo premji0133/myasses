8c55655ca0048664bca6da99e14fb8c0d32286ba
premji0133 <30617577+premji0133@users.noreply.github.com>
2017-12-05 11:21:10 +0530

    Update data.service.ts

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,21 +1,20 @@
 import { Injectable }    from '@angular/core';
 import { Headers, Http, Response, RequestOptionsArgs } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
-import { marks } from './mock';
-import { markk } from './mock';
+import { mark } from './mock';
+
 @Injectable()
 export class DataService 
 {
    constructor(private http: Http) { }
   
-   getmarks()  : Promise<markk> {
+   getmarks()  : Promise<mark[]> 
+  {
     
-
-
                return this.http.get('https://assespmkvy.herokuapp.com/api/public/list_mark')
                   .toPromise()
-                  .then(res => <markk> res.json(), this.handleError)
-                  .then(data => {  return data; }); 
+                  .then(res => <mark[]> res.json(), this.handleError)
+                  .then(data => { console.log(data); return data; }); 
 
 
    }
@@ -24,7 +23,8 @@ export class DataService
   
    
    
-     private handleError(error: any): Promise<any> {
+     private handleError(error: any): Promise<any> 
+  {
     console.error('An error occurred', error); // for demo purposes only
     return Promise.reject(error.message || error);
   }
