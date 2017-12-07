a78cb31fd412a51d8cc29e00ccdd4acd20de63e3
premji0133 <30617577+premji0133@users.noreply.github.com>
2017-12-07 16:37:24 +0530

    Update dash.ts

--- src/app/dash/dash.ts
+++ src/app/dash/dash.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormGroup, FormControl, Validators, FormBuilder }  from '@angular/forms';
+import { AuthGuard } from '../services/auth.guard';
 @Component({
   selector: 'app-dash',
   templateUrl: './dash.html',
   styleUrls: ['./dash.css']
 })
 export class DashComponent implements OnInit {
-  constructor( private router: Router) {   }
+  constructor( private router: Router private ag: AuthGuard) {   }
   ngOnInit()  {}
- login() {
-   
-   
-   
- }
+ login() { }
+  logout()
+  {
+    this.ag.token=false;
   }
+}
